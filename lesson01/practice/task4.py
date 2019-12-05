12621df854c6b5e2034770e54988b238c2c9103e
pogrebnyak <55633045+pogrebnyak@users.noreply.github.com>
2019-12-05 12:13:04 +0200

    Create task4.py

---
+++ lesson01/practice/task4.py
@@ -0,0 +1,12 @@
+# При условии что процент добавляется в телу депозита.
+def bank(deposit_amount, number_years, percent):
+    amount = deposit_amount
+    for year in range(1, number_years + 1):
+        amount *= 1 + percent/100
+        print(year, amount)
+
+    return round(amount, 2)
+
+print(bank(1000, 10, 10))
+print(bank(10000, 5, 5))
+print(bank(3000, 3, 2))
