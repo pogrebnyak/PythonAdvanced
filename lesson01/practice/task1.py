5e6512e57a3a57d3fc080f66f7ab02026886e5f8
pogrebnyak <55633045+pogrebnyak@users.noreply.github.com>
2019-12-05 11:34:22 +0200

    Update task1.py

--- lesson01/practice/task1.py
+++ lesson01/practice/task1.py
@@ -1,5 +1,6 @@
 def even_numbers(n):
-    return [number for number in range(n + 1)][::2]
+    list_numbers = [number for number in range(n + 1)]
+    return list_numbers[::2]
 
 print(even_numbers(30))
-print(even_numbers(9))
+print(even_numbers(19))
